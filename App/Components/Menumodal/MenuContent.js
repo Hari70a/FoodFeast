170d056b8fba65bce43d59e3f1bc55a95b0ac698
Hari70a <raja70a@gmail.com>
2018-12-07 01:11:23 +0630

    Nav header and Menu modal added(filterby done partially)

---
+++ App/Components/Menumodal/MenuContent.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Modal,
+  TouchableHighlight,
+  Dimensions,
+  TouchableOpacity
+} from "react-native";
+import { colors } from "../../Config";
+import Loading from "../Loading";
+const menuItems = [
+  { id: 1, title: "Price low to high" },
+  { id: 2, title: "Rating" }
+];
+
+export default class MenuContent extends Component {
+  render() {
+    return (
+      <View>
+        <View style={styles.headerContainer}>
+          <TouchableHighlight
+            style={{ backgroundColor: "green", padding: 15 }}
+            underlayColor={"green"}
+            onPress={() => this.props.closeModal()}
+          >
+            <Text>Close</Text>
+          </TouchableHighlight>
+        </View>
+        <View style={styles.contentContainer}>
+          {menuItems.map((txt, index) => {
+            return (
+              <TouchableOpacity
+                style={styles.menu}
+                key={index}
+                onPress={() => this.props.filterBy(txt.id)}
+              >
+                <Text style={styles.menuTxt}>{txt.title}</Text>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </View>
+    );
+  }
+}
+
+const center = { alignItems: "center", justifyContent: "center" };
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.bgColor
+  },
+  modalContainer: {
+    height: 300,
+    width: Dimensions.get("window").width - 40,
+    backgroundColor: colors.bgColor,
+    ...center
+  },
+  modalOuterContainer: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    ...center
+  },
+  headerContainer: {
+    flex: 0.2,
+    alignItems: "flex-end",
+    justifyContent: "center",
+    width: Dimensions.get("window").width - 40
+  },
+  contentContainer: {
+    flex: 0.8,
+    marginTop: 15,
+    width: Dimensions.get("window").width - 40
+  },
+  menu: {
+    marginHorizontal: 15,
+    borderColor: colors.txtColor,
+    borderBottomWidth: 1,
+    padding: 10
+  },
+  menuTxt: {
+    color: colors.txtColor,
+    fontSize: 18
+  }
+});
---
+++ App/Components/Menumodal/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Modal,
+  TouchableHighlight,
+  Dimensions
+} from "react-native";
+import { colors } from "../../Config";
+import Loading from "../Loading";
+import MenuContent from "./MenuContent";
+
+export default class Menumodal extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={this.props.modalVisible}
+          onRequestClose={() => {
+            this.props.closeModal();
+          }}
+        >
+          <View style={{ flex: 1 }}>
+            <View style={styles.modalOuterContainer}>
+              <View style={styles.modalContainer}>
+                <MenuContent
+                  closeModal={this.props.closeModal}
+                  filterBy={this.props.filterBy}
+                />
+              </View>
+            </View>
+          </View>
+        </Modal>
+      </View>
+    );
+  }
+}
+
+const center = { alignItems: "center", justifyContent: "center" };
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.bgColor
+  },
+  modalContainer: {
+    height: 300,
+    width: Dimensions.get("window").width - 40,
+    backgroundColor: colors.bgColor,
+    ...center
+  },
+  modalOuterContainer: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
