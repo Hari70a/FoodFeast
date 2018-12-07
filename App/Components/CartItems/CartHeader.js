abfd687b658414c07a6f02a733619d5a9c2f3f49
Hari70a <raja70a@gmail.com>
2018-12-07 23:14:16 +0630

    Cart Header component added

---
+++ App/Components/CartItems/CartHeader.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Platform
+} from "react-native";
+import { colors } from "../../Config";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <View style={styles.homeHeaderContainer}>
+        <TouchableOpacity
+          style={styles.backViewContainer}
+          onPress={() => this.props.moveToPrevious()}
+        >
+          <Text>Back</Text>
+        </TouchableOpacity>
+        <View style={styles.homeHeaderView}>
+          <Text style={styles.homeHeaderText}>{this.props.title}</Text>
+        </View>
+        <View style={{ flex: 0.2, flexDirection: "row" }} />
+      </View>
+    );
+  }
+}
+
+const center = { alignItems: "center", justifyContent: "center" };
+const styles = StyleSheet.create({
+  homeHeaderContainer: {
+    flex: 0.1,
+    height: 55,
+    backgroundColor: colors.bgColor,
+    alignItems: "center",
+    flexDirection: "row",
+    paddingTop: Platform.OS == "ios" ? 12 : 0,
+    elevation: 5
+  },
+  backViewContainer: {
+    flex: 0.2,
+    height: 55,
+    ...center
+  },
+  homeHeaderText: {
+    color: colors.headerColor,
+    fontWeight: "bold",
+    fontSize: 18
+  },
+  homeHeaderView: {
+    flex: 0.6,
+    alignItems: "center"
+  },
+  homeIconView: {
+    flex: 0.1,
+    padding: 10,
+    ...center
+  },
+  filterbyStyle: {
+    flex: 0.5,
+    marginRight: 5,
+    padding: 10,
+    ...center
+  }
+});
