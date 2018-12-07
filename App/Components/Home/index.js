87dfdebdcb800285758f03161ac9bf1319d21e29
Hari70a <raja70a@gmail.com>
2018-12-07 22:38:24 +0630

    Filter By option added in Home screen

--- App/Components/Home/index.js
+++ App/Components/Home/index.js
@@ -7,13 +7,22 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Modal,
+  TouchableHighlight,
+  Dimensions
+} from "react-native";
 import { foodItems, colors } from "../../Config";
 import Recipe from "../Recipe";
 import Loading from "../Loading";
+import MenuModal from "../Menumodal";
 import Header from "../Header";
 import APIHandlers from "../../Services/APIHandlers";
-import MenuModal from "../Menumodal";
 
 export default class Home extends Component {
   constructor(props) {
@@ -33,47 +42,60 @@ export default class Home extends Component {
   _keyExtractor = (item, index) => `${item.id}`;
 
   toggleModal = () => {
+    this.setState({ modalVisible: !this.state.modalVisible });
+  };
+
+  filterByPrice = () => {
+    const clonedRes = this.state.foodItems.slice();
+    clonedRes.sort((first, second) => first.item_price - second.item_price);
+    this.setState({ foodItems: clonedRes, isLoading: false });
+  };
+
+  filterByRating = () => {
+    const clonedRes = [...this.state.foodItems];
+    clonedRes.sort((a, b) => b.average_rating - a.average_rating);
     this.setState({
-      modalVisible: !this.state.modalVisible
+      isLoading: false,
+      foodItems: clonedRes
     });
   };
 
   filterBy = id => {
-    console.log("filterBY");
-    // if(id=== 1) this.filterByPrice()
-    // else this.filterByRating();
+    this.toggleModal();
+    this.setState({ isLoading: true });
+    if (id === 1) {
+      this.filterByPrice();
+    } else {
+      this.filterByRating();
+    }
   };
 
   render() {
-    console.log(this.props, this.state, "Home Props");
     if (this.state.isLoading) {
       return <Loading />;
     }
     return (
       <View style={styles.container}>
-        <View style={{ flex: 0.2 }}>
-          <Header title={"FoodFeast"} toggleModal={this.toggleModal} />
-        </View>
-        <View style={{ flex: 0.8 }}>
-          <MenuModal
-            toggleModal={this.toggleModal}
-            filterBy={this.filterBy}
-            modalVisible={this.state.modalVisible}
-            closeModal={() => this.setState({ modalVisible: false })}
-          />
-          <FlatList
-            data={this.state.foodItems}
-            renderItem={({ item }) => (
-              <Recipe {...item} navigation={this.props.navigation} />
-            )}
-            keyExtractor={this._keyExtractor}
-          />
-        </View>
+        <Header title={"FoodFeast"} toggleModal={this.toggleModal} />
+        <MenuModal
+          modalVisible={this.state.modalVisible}
+          toggleModal={this.toggleModal}
+          closeModal={() => this.setState({ modalVisible: false })}
+          filterBy={this.filterBy}
+        />
+        <FlatList
+          data={this.state.foodItems}
+          renderItem={({ item }) => (
+            <Recipe {...item} navigation={this.props.navigation} />
+          )}
+          keyExtractor={this._keyExtractor}
+        />
       </View>
     );
   }
 }
 
+const center = { alignItems: "center", justifyContent: "center" };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
